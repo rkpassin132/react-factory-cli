cfc15d1eb76833e47600138733ff5a3f30a8ee6d
Rahul kumar <rkpassin132@gmail.com>
2024-09-29 15:46:53 +0530

    bug(delete temp-folder): add delay to delete temp folder

--- src/helper/gitHelper.ts
+++ src/helper/gitHelper.ts
@@ -20,7 +20,9 @@ export const gitClone = async (branch: string, targetDir: string) => {
             const dest = path.join(targetDir, file);
             await fs.renameSync(src, dest);
         }
-        fs.rmSync(tempDir, { recursive: true, force: true });
+        setTimeout(() => {
+            fs.rmSync(tempDir, { recursive: true, force: true });
+        }, 100);
         logger.info("Application created");
 
         logger.info("Installing packages using `npm install`");
